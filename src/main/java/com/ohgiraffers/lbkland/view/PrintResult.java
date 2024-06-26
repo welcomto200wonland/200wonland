51e587172a8d1a90763d26f5d451cf22e90a3808
dmsgpk237 <a01027947353@gmail.com>
2024-06-26 14:43:42 +0900

    [fix] commit

--- src/main/java/com/ohgiraffers/lbkland/view/PrintResult.java
+++ src/main/java/com/ohgiraffers/lbkland/view/PrintResult.java
@@ -1,4 +1,30 @@
 package com.ohgiraffers.lbkland.view;
 
 public class PrintResult {
+
+    public void printSuccessMessage(String successCode) {
+
+        String successMessage = "";
+
+        switch ((successCode)){
+            case "insert" : successMessage = "insert에 성공했습니다."; break;
+            case "update" : successMessage = "update에 성공했습니다."; break;
+            case "delete" : successMessage = "delete에 성공했습니다."; break;
+        }
+        System.out.println(successMessage);
+    }
+
+    public void printErrorMessage(String errorCode){
+
+        String errorMessage= "";
+        switch (errorCode){
+            case "selectList" : errorMessage = "전체 조회에 실패했습니다."; break;
+            case "selectOne" : errorMessage = "단일 조회에 실패했습니다."; break;
+            case "insert" : errorMessage = "insert에 실패했습니다."; break;
+            case "update" : errorMessage = "update에 실패했습니다."; break;
+            case "delete" : errorMessage = "delete에 실패했습니다."; break;
+        }
+        System.out.println(errorMessage);
+    }
+
 }
