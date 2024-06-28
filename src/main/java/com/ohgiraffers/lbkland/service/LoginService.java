310494d195e0d6b616fd8f75a19e39953596cbdc
HEun0420 <usialeta@gmail.com>
2024-06-28 15:49:49 +0900

    [fix] 머지 수정

--- src/main/java/com/ohgiraffers/lbkland/service/LoginService.java
+++ src/main/java/com/ohgiraffers/lbkland/service/LoginService.java
@@ -24,7 +24,7 @@ public class LoginService {
         // 입력받은 id로 회원 한명의 정보를 가져오기
         ConsumerDTO foundConsumer = consumerLoginMapper.findConsumerById(consumer.getConsumerId());
 
-        // 찾아온 회원의 비밀번호와 입력받은 비밀번호가 일치하는지 확인
+        // 찾아온 회원의 비밀번호(found)와 입력받은 비밀번호(consumer)가 일치하는지 확인
         boolean result = foundConsumer.getConsumerPw().equals(consumer.getConsumerPw());
 
         sqlSession.close();
@@ -55,7 +55,7 @@ public class LoginService {
 
         consumerLoginMapper = sqlSession.getMapper(ConsumerLoginMapper.class);
 
-        // 입력받은 id로 회원 한명의 정보를 가져오기
+        // 입력받은 id로 회원 한명의 정보를 가져와서 foundConsumer에 저장
         ConsumerDTO foundConsumer = consumerLoginMapper.findConsumerById(consumer.getConsumerId());
 
         // 찾아온 회원의 비밀번호와 입력받은 비밀번호가 일치하는지 확인
